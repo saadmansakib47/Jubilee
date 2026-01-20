b1bc5971f2aff81ebe7cedc7eede6183922fe356
unknown <saadmansakib@iut-dhaka.edu>
2026-01-20 17:06:59 +0600

    chore : update gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,3 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
- */
-
-/**
- * @type {import('gatsby').GatsbyConfig}
- */
 module.exports = {
   siteMetadata: {
     title: `Tasmiah Nawal`,
@@ -15,6 +6,9 @@ module.exports = {
     siteUrl: `https://tasmiahnawal.com/`,
   },
   plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-postcss`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -22,24 +16,36 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    "gatsby-plugin-image",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    "gatsby-plugin-postcss",
-    "gatsby-plugin-offline",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/src/content`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `Tasmiah Nawal`,
         short_name: `Tasmiah`,
         start_url: `/`,
-        background_color: "#FFFFFF",
-        theme_color: "#5B2D8B",
+        background_color: `#5B2D8B`,
+        theme_color: `#5B2D8B`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`,
+        icon: `src/images/icon.png`, //add icon here
       },
     },
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.mdx`, `.md`],
+      },
+    },
+    `gatsby-transformer-remark`,
+    // Uncomment for offline support
+    // `gatsby-plugin-offline`,
   ],
 }
